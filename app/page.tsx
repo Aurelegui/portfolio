b091d0beea38cc100e3637b2e9c552c32a279322
Aurelegui <aurele.guitard@gmail.com>
2024-03-21 13:54:02 +0100

    added navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import { ModeToggle } from "@/components/ui/modeToggle";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ModeToggle />
-      <p>Hi</p>
     </main>
   );
 }
